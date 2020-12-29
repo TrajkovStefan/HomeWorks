b4a651d6f5880138ef45fd65fec1254d389535b6
Stefan <75165862+TrajkovStefan@users.noreply.github.com>
2020-12-29 03:43:36 +0100

    Add files via upload

---
+++ JavaScript HomeWorks/homework#3/part2.js
@@ -0,0 +1,10 @@
+function parameter(a,b,c,d,e){
+    console.log(typeof(a));
+    console.log(typeof(b));
+    console.log(typeof(c));
+    console.log(typeof(d));
+    console.log(typeof(e));
+}
+
+parameter(null,true,2,"Hi");
+
