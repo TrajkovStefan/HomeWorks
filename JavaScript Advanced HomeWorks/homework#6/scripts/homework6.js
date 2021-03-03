0b5db7f2f0dd846d41b6ecba4fb1e085fe44a204
Stefan <75165862+TrajkovStefan@users.noreply.github.com>
2021-03-03 23:01:18 +0100

    updated

--- JavaScript Advanced HomeWorks/homework#6/scripts/homework6.js
+++ JavaScript Advanced HomeWorks/homework#6/scripts/homework6.js
@@ -1,77 +1,60 @@
-//TASK 1
-let library = {
-    name: "Boston Public Library",
-    books: ["JavaScriptAdvanced", "JavaScriptBasic", "C#", ".Net", "Java"],
-    address: "Las Vegas",
-    numberOfMembers: function(){
-       return this.books.length * 15;
-    },
-    printBooks: function(){
-        Object.values(this.books).forEach(book => console.log(book));
+function Library(name,books,address){
+    this.name = name;
+    this.books = books;
+    this.address = address;
+    this.numberOfMembers = this.books.length * 15;
+    this.printBooks = function(){
+       this.books.forEach(book => console.log(book.title));
     }
 }
 
-let book = {
-    title: "Last chance",
-    genre: "romance",
-    libraries: ["Bodleian Library", "Vatican Library", "Boston Public Library", "Thomas Fisher"],
-    authors: ["William Shakespeare", "Harold Robbins", "Dean Koontz", "Paulo Coelho"],
-    addLibrary: function(){
-        return library.books.push(this.title);
-    },
-    removeLibrary: function(){
-        return library.books.pop(this.title);
+function Book(title,genre,libraries,authors){
+    this.title = title;
+    this.genre = genre;
+    this.libraries = libraries;
+    this.authors = authors;
+    this.addLibrary = function(someBook){
+       someBook.books.push(this)
+    }
+    this.removeLibrary = function(someBook){
+       someBook.books.pop(this);
     }
 }
 
-let author = {
-    firstName: "William",
-    lastName: "Shakespeare",
-    yearOfBirth: 1970,
-    books: [],
-    currentBook: null,
-    startBook: function(newBook){
-        if(this.currentBook === null || undefined){
-            return this.books.push(book.title);
+function Author(firstName,lastName,YearOfBirth){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.YearOfBirth = YearOfBirth;
+    this.books = [];
+    this.currentBook = null;
+    this.startBook = function(someBook){
+        this.books.push(someBook); // Book added to the Author property Books
+        if(this.currentBook !== someBook){ // if was another book in the currentBook
+            this.books.push(this.currentBook); // book transfered to books
         }
         else{
-            
-            return (this.books.push(this.currentBook) && (this.currentBook = newBook));
+            this.currentBook = someBook; //then add newBook as currentBook
         }
-    },
+    }
 }
 
-console.log("===All books of object library===");
-library.printBooks();
+let author = new Author("Wiliam", "Shakespeare", 1970);
+let book = new Book("Python", "programming", ["Boston Public Library", "Vatican Library"], [author]);
+let library = new Library("Boston Public Library", [], "Las Vegas");
 
-//TASK 2
-console.log("===TASK 2===");
 
-//addLibrary()
-book.addLibrary();
-console.log(library.books);
+//Add Book
+book.addLibrary(library);
+//Pint All Books
+library.printBooks(book);
 
-//removeLibrary()
-book.removeLibrary();
-console.log(library.books);
+//Remove Book
+book.removeLibrary(library);
+//Print All Books
+library.printBooks(book);
 
-//startBook();
-author.startBook(book.title);
+
+author.startBook(book);
 console.log(author.books);
 console.log(author.currentBook);
-
-//TASK3 
-console.log("===TASK 3===");
-
-let newBook = Object.create(library);
-newBook.addBook = function(){
-    return this.books.push(book.title);
-}
-
-newBook.addBook();
-console.log(newBook.books);
-console.log(library.books);
-
-
-
 
